aacc6b06f4ddae02f54af2b3ec1f8683204cc88b
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-06 17:40:35 +0530

    conversions

--- javascript/one.js
+++ javascript/one.js
@@ -3,3 +3,7 @@ let name ="mohit";
 let role="Student";
 console.table([id,name,role]) //for printing data in tabular form 
 
+// for convertng anything into number
+let value="35" //it a string
+let convert =Number(value)
+console.log(typeof convert) //by doing this we can convert a string into number 
