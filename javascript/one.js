e1d01b7fd73c460ee5be2afb599535f3f9ac01e2
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-06 17:41:30 +0530

    conversions of datatypes

--- javascript/one.js
+++ javascript/one.js
@@ -3,7 +3,15 @@ let name ="mohit";
 let role="Student";
 console.table([id,name,role]) //for printing data in tabular form 
 
-// for convertng anything into number
-let value="35" //it a string
+// for convertng anything into number 
+//as like this we can convert anything to any means string to number , number to string , boolean to number and vv
+let value="35a" //it a string
 let convert =Number(value)
 console.log(typeof convert) //by doing this we can convert a string into number 
+console.log(convert)//it gives NAN means not number
+// 33 = number
+//33abc =nan
+//null =0
+//true =1
+//false =0
+//undefined=nan 
