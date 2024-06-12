c59c0ed1210536249ebc42d283244a37b1ec1bb2
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-12 21:33:01 +0530

    iife

--- javascript/third.js
+++ javascript/third.js
@@ -1,2 +1,8 @@
 //immediatly invoke function Expression
- 
+(function first_IIFE() {
+  console.log("iffe means calle fuction  just after its execution ");
+})(); //when we write two iffem function at same so we need to use semicolon after first so that its execution stops
+
+((name) => {
+  console.log(` iffe using arrow function name of function is ${name} `);
+})("Arrow function using iffe");
