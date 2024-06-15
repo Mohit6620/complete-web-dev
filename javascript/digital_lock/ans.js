e04f2e68fe0cc410ad0c8457ca898ae2d88fbb84
Mohit6620 <work.mohitvarade6620@gmail.com>
2024-06-15 21:52:20 +0530

    adding function for clock

--- javascript/digital_lock/ans.js
+++ javascript/digital_lock/ans.js
@@ -1,12 +1,22 @@
-let mydate = new Date(); //it is a object
-let date = String(mydate);
-let final = mydate.toDateString();
-let time = mydate.toTimeString();
-console.log(final);
-console.log(time);
-const digi = time.split(" ")[0];
+//it is also a method but it dose not give us changing time , time changes with refreshing of the pages
+// let mydate = new Date(); //it is a object
+// let date = String(mydate);
+// let final = mydate.toDateString();
+// let time = mydate.toTimeString();
+// console.log(final);
+// console.log(time);
+// const digi = time.split(" ")[0];
 
-const div = document.querySelector("#clock");
-const date_1 = document.querySelector("#date");
-div.innerHTML = digi;
-date_1.innerHTML = final;
+// const div = document.querySelector("#clock");
+// const date_1 = document.querySelector("#date");
+// div.innerHTML = mydate.toLocaleTimeString();
+// date_1.innerHTML = final;
+
+setInterval(function () {
+  //with using this funvtion we can see the running values
+  let mydate = new Date();
+  const div = document.querySelector("#clock");
+  const date_1 = document.querySelector("#date");
+  div.innerHTML = mydate.toLocaleTimeString();
+  date_1.innerHTML = final;
+});
